d5eb9805dd1a4a85827cbcf58d8aa4d2d41826d5
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-14 20:36:19 +0200

    Cashing

---
+++ Caching/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Caching.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [OutputCache(Duration = 10, VaryByParam= "*", Location =System.Web.UI.OutputCacheLocation.Downstream, NoStore =true)]
+
+        public ActionResult Index()
+        {
+            ViewBag.CurrentExecutedDateTime = DateTime.Now.ToString("hh:mm:ss:fff");
+            return View();
+        }
+
+        //[OutputCache(Duration = 10)]
+        public ActionResult DonutHoleCache()
+        {
+            ViewBag.LastExecutedDateTime = DateTime.Now.ToString("hh:mm:ss:fff");
+            return View("_DonutHole");
+        }
+    
+}
+}
